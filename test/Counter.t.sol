899e7cc3de91ac7178b50d576f23f3e4b073fc21
beskay <beskay0x@gmail.com>
2023-10-07 16:37:49 +0200

    Add test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,156 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import "src/interfaces/ICounter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract CounterTestBase is Test {
+    ICounter internal counter;
+
+    function deploy() internal virtual returns (address addr) {
+        // first: get the bytecode from the environment if it exists
+        bytes memory empty = new bytes(0);
+        bytes memory bytecode = vm.envOr("BYTECODE", empty);
+
+        if (bytecode.length > 0) {
+            assembly {
+                addr := create(0, add(bytecode, 0x20), mload(bytecode))
+            }
+            return addr;
+        }
+    }
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        counter = ICounter(deploy());
+    }
+
+    function test_GetNumber() public {
+        assertEq(counter.getNumber(), 0);
     }
 
     function test_Increment() public {
         counter.increment();
-        assertEq(counter.number(), 1);
+        assertEq(counter.getNumber(), 1);
     }
 
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
-        assertEq(counter.number(), x);
+        assertEq(counter.getNumber(), x);
+    }
+}
+
+/*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+/*                          SOLIDITY                          */
+/*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+contract CounterTestSol is CounterTestBase {
+    function deploy() internal override returns (address addr) {
+        // The string array input variable used by ffi
+        string[] memory deployCommand = new string[](4);
+        // We get the bytecode of the Solidity contract with
+        // forge inspect <contract> bytecode
+        deployCommand[0] = "forge";
+        deployCommand[1] = "inspect";
+        deployCommand[2] = "Counter";
+        deployCommand[3] = "bytecode";
+
+        // A local variable to hold the output bytecode
+        bytes memory compiledByteCode = vm.ffi(deployCommand);
+
+        // A local variable to hold the address of the deployed Solidity contract
+        address deployAddr;
+
+        // Inline assembly code to deploy a contract using bytecode
+        assembly {
+            deployAddr := create(0, add(compiledByteCode, 0x20), mload(compiledByteCode))
+        }
+
+        return deployAddr;
+    }
+}
+
+/*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+/*                            YUL                             */
+/*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+contract CounterTestYul is CounterTestBase {
+    function deploy() internal override returns (address addr) {
+        // The string array input variable used by ffi
+        string[] memory deployCommand = new string[](4);
+        // We get the bytecode of the Yul contract with
+        // forge inspect <contract> bytecode
+        deployCommand[0] = "forge";
+        deployCommand[1] = "inspect";
+        deployCommand[2] = "Counter_Yul";
+        deployCommand[3] = "bytecode";
+
+        // A local variable to hold the output bytecode
+        bytes memory compiledByteCode = vm.ffi(deployCommand);
+
+        // A local variable to hold the address of the deployed Yul contract
+        address deployAddr;
+
+        // Inline assembly code to deploy a contract using bytecode
+        assembly {
+            deployAddr := create(0, add(compiledByteCode, 0x20), mload(compiledByteCode))
+        }
+
+        return deployAddr;
+    }
+}
+
+/*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+/*                           VYPER                            */
+/*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+contract CounterTestVy is CounterTestBase {
+    function deploy() internal override returns (address addr) {
+        // The string array input variable used by ffi
+        string[] memory deployCommand = new string[](2);
+        // The Vyper keyword to compile a contract
+        deployCommand[0] = "vyper";
+        // The path to the Vyper contract file starting from the project root directory
+        deployCommand[1] = "src/Counter.vy";
+
+        // A local variable to hold the output bytecode
+        bytes memory compiledByteCode = vm.ffi(deployCommand);
+
+        // A local variable to hold the address of the deployed Vyper contract
+        address deployAddr;
+
+        // Inline assembly code to deploy a contract using bytecode
+        assembly {
+            deployAddr := create(0, add(compiledByteCode, 0x20), mload(compiledByteCode))
+        }
+
+        return deployAddr;
+    }
+}
+
+/*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+/*                            HUFF                            */
+/*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+contract CounterTestHuff is CounterTestBase {
+    function deploy() internal override returns (address addr) {
+        // The string array input variable used by ffi
+        string[] memory deployCommand = new string[](3);
+        // The Vyper keyword to compile a contract
+        deployCommand[0] = "huffc";
+        // The path to the Vyper contract file starting from the project root directory
+        deployCommand[1] = "src/Counter.huff";
+        deployCommand[2] = "--bytecode";
+
+        // A local variable to hold the output bytecode
+        bytes memory compiledByteCode = vm.ffi(deployCommand);
+
+        // A local variable to hold the address of the deployed Vyper contract
+        address deployAddr;
+
+        // Inline assembly code to deploy a contract using bytecode
+        assembly {
+            deployAddr := create(0, add(compiledByteCode, 0x20), mload(compiledByteCode))
+        }
+
+        return deployAddr;
     }
 }
