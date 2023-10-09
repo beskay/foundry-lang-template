68ee0327f2f7387ff2ee87d0b7766768e60b1fa2
beskay <beskay0x@gmail.com>
2023-10-09 09:07:04 +0200

    :bulb: Update comments in Huff test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -135,16 +135,16 @@ contract CounterTestHuff is CounterTestBase {
     function deploy() internal override returns (address addr) {
         // The string array input variable used by ffi
         string[] memory deployCommand = new string[](3);
-        // The Vyper keyword to compile a contract
+        // The Huff keyword to compile a contract
         deployCommand[0] = "huffc";
-        // The path to the Vyper contract file starting from the project root directory
+        // The path to the Huff contract file starting from the project root directory
         deployCommand[1] = "src/Counter.huff";
         deployCommand[2] = "--bytecode";
 
         // A local variable to hold the output bytecode
         bytes memory compiledByteCode = vm.ffi(deployCommand);
 
-        // A local variable to hold the address of the deployed Vyper contract
+        // A local variable to hold the address of the deployed Huff contract
         address deployAddr;
 
         // Inline assembly code to deploy a contract using bytecode
